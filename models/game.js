8c41dc5e5b2d07a0d133368e5ab1eda4b131e724
Sercan Değirmenci <degirmencisercan@gmail.com>
2015-06-09 08:05:34 +0300

    :fire: a few game api added

--- models/game.js
+++ models/game.js
@@ -8,7 +8,7 @@ var ObjectID = require('mongodb').ObjectID;
 var db = require('../lib/db');
 
 
-export class Builds {
+export class Build {
     constructor(version, url, news) {
         this.version = version;
         this.download = url;
@@ -19,8 +19,12 @@ export class Builds {
 export class Game {
 
     constructor(owner, name) {
+        let o_id = owner;
+        if (typeof id === 'string') {
+            o_id = new ObjectID(owner);
+        }
         this._id = new ObjectID();
-        this.owner = owner;
+        this.owner = o_id;
         this.name = name;
         this.keywords = [];
         this.website = "";
@@ -37,4 +41,32 @@ export class Game {
     save(cb) {
         db.game.insert(this, cb);
     }
+
+    static findById(id, opt, done) {
+        if (typeof done === 'undefined') {
+            done = opt;
+            opt = {};
+        }
+
+        var _id = id;
+        if (typeof id === 'string') {
+            _id = new ObjectID(id);
+        }
+        db.game.findOne({_id: _id}, opt, function (err, doc) {
+            if (err) {
+                return done(err);
+            } else {
+                return done(null, doc);
+            }
+        });
+    }
+
+    static updateById(id, data, cb) {
+        var _id = id;
+        if (typeof id === 'string') {
+            _id = new ObjectID(id);
+        }
+        return db.game.update({_id: _id}, data, cb);
+    }
+
 }
--- routes/api/game.js
+++ routes/api/game.js
@@ -7,10 +7,103 @@
 
 var express = require('express');
 var router = express.Router();
+var validator = require('validator');
+var Game = require('../../models/game').Game;
+var Build = require('../../models/game').Build;
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/:id', function (req, res, next) {
+    let game_id = req.params.id;
+
+    if (!validator.isMongoId(game_id)) {
+        return next(new Error("Invalid Game ID"));
+    }
+
+    Game.findById(game_id, (err, doc)=> {
+        if (err) {
+            return next(err);
+        }
+        if (!doc) {
+            return next(new Error('Game not found'));
+        }
+        return res.status(200).json({
+            success: true,
+            data: doc
+        });
+    });
+});
+
+router.get('/:id/getsettings', function (req, res, next) {
     res.send("there is nothing to see here!");
+});
+
+router.post('/create', function (req, res, next) {
+    let name = req.body.name;
+    let owner = req.body.owner;
+
+    if (!name) {
+        return next(new Error("Name of the game is missing"));
+    }
+    if (!owner) {
+        return next(new Error("Owner ID is missing"));
+    }
+    if (!validator.isMongoId(owner)) {
+        return next(new Error("Invalid Owner ID"));
+    }
+    let game = new Game(owner, name);
+
+    game.save((err)=> {
+        if (err) {
+            return next(err);
+        }
+        res.status(200).json({
+            success: true,
+            game_id: game._id
+        });
+    });
+});
+
+function getValidUpdateData(data) {
+    if (data._id) {
+        delete data._id;
+    }
+    if (data.builds) {
+        delete data.builds;
+    }
+
+    return {
+        $set: data
+    };
+}
+
+router.post('/:id/update', function (req, res, next) {
+    let game_id = req.params.id;
+
+    if (!validator.isMongoId(game_id)) {
+        return next(new Error("Invalid Game ID"));
+    }
+
+    Game.findById(game_id, (err, doc)=> {
+        if (err) {
+            return next(err);
+        }
+        if (!doc) {
+            return next(new Error('Game not found'));
+        }
+        let data = getValidUpdateData(req.body);
+        if (!data) {
+            return next(new Error('Invalid Update Data'));
+        } else {
+            return Game.updateById(game_id, data, (err2)=> {
+                if (err2) {
+                    return next(err2);
+                } else {
+                    return res.status(200).json({
+                        success: true
+                    });
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
