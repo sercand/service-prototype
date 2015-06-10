cbbdee0ce474c39e1aee261f5203a1c25df51510
Sercan Değirmenci <degirmencisercan@gmail.com>
2015-06-10 13:22:40 +0300

    new stat apis

--- routes/api/stat.js
+++ routes/api/stat.js
@@ -57,13 +57,55 @@ function AnswersByGame(req, res, next) {
 }
 
 function PlayTime(req, res, next) {
+    let parent_id = req.params.id;
 
+    if (!validator.isMongoId(parent_id)) {
+        return next(new Error("Invalid Parent ID"));
+    }
+
+    var pid = new ObjectID(parent_id);
+    Stat.aggregate([
+        {$match: {parent_id: pid, event: Stat.Events.GameEnded}},
+        {$project: {event: "$event", value: "$value", hour: {$hour: "$date"}}},
+        {$group: {_id: {time: "$hour"}, value: {$sum: "$value"}}}], function (err, docs) {
+        if (err) {
+            return next(err);
+        }
+        res.status(200).json({success: true, data: docs});
+    });
+}
+
+function PlayTimeByGame(req, res, next) {
+    let parent_id = req.params.id;
+    let game_id = req.params.game_id;
+
+    if (!validator.isMongoId(parent_id)) {
+        return next(new Error("Invalid Parent ID"));
+    }
+    if (!validator.isMongoId(game_id)) {
+        return next(new Error("Invalid Game ID"));
+    }
+
+    var pid = new ObjectID(parent_id);
+    var gid = new ObjectID(game_id);
+
+    Stat.aggregate([
+        {$match: {parent_id: pid, game_id: gid, event: Stat.Events.GameEnded}},
+        {$project: {event: "$event", value: "$value", hour: {$hour: "$date"}}},
+        {$group: {_id: {time: "$hour"}, value: {$sum: "$value"}}}], function (err, docs) {
+        if (err) {
+            return next(err);
+        }
+        res.status(200).json({success: true, data: docs});
+    });
 }
 
 router.get('/allanswers/:id', AllAnswers);
 
 router.get('/allanswers/:id/:game_id', AnswersByGame);
 
-router.get('/playtime/:id', AllAnswers);
+router.get('/playtime/:id', PlayTime);
+
+router.get('/playtime/:id/:game_id', PlayTimeByGame);
 
 module.exports = router;
